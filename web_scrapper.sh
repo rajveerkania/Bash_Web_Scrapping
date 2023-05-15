640e069938f0db248031f7bcd87bfb9a9ac4935a
rajveerkania <rajveerkania02@gmail.com>
2023-05-15 23:59:13 +0530

    Need changes in the printing order

--- web_scrapper.sh
+++ web_scrapper.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
+
+# Extracting the html file from the server
 curl -s -o output.txt "https://techcrunch.com/"
 
-grep -A 1 "post-block__title__link" output.txt | tail -n +6 > output2.txt
+# Extracting the lines containing having <a> and </a> with class="post-block__title__link"
+grep -A 1 "post-block__title__link" output.txt | tail -n +6 | sed 's/^[[:space:]]*//' - > output2.txt
 
+#Storing the data for inside a variable for the while loop
 file_content=$(cat output2.txt)
+
+#Initialising an array for iterating over title
+array=()
+while read -r line; do
+  array+=( "$line" )
+done < <(cat output2.txt | grep '^[A-Z]' | sed 's/<\/a>//g')
+
+#Flag variable for iterating in the while loop
+flag=0
 
 while IFS='<' read -r line;
 do
-  text=$(sed -n 's/.*<a href="[^>]*>\([^<]*\)<\/a>.*/\1/p' output2.txt)
-  echo "$text"
+	echo "Title: ${array[$flag]}"
+	flag=$((flag + 1))
   
   if [[ $line =~ href=\"(.*)\".* ]]; then
     link=$(echo "${BASH_REMATCH[1]}" | sed 's/" class="post-block__title__link//g')
@@ -18,4 +31,4 @@ do
 done <<< "$file_content"
 
 
-rm output.txt output2.txt
+rm output.txt output2.txt output3.txt
