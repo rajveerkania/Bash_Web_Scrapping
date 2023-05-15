6016c29591cd20219d3a241b5c390447577f7696
rajveerkania <rajveerkania02@gmail.com>
2023-05-15 17:51:08 +0530

    Links are now available

--- web_scrapper.sh
+++ web_scrapper.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-echo "TODAY'S NEWS:"
+curl -s -o output.txt "https://techcrunch.com/"
 
-# Scrapping the data and storing it into the output.txt file
-text="`curl -s "https://techcrunch.com/" | grep -A 1 'class="post-block__title__link"'`"
+grep -A 1 "post-block__title__link" output.txt | tail -n +6 > output2.txt
 
-echo $text
+file_content=$(cat output2.txt)
 
+while IFS='<' read -r line;
+do
+  text=$(sed -n 's/.*<a href="[^>]*>\([^<]*\)<\/a>.*/\1/p' output2.txt)
+  echo "$text"
+  
+  if [[ $line =~ href=\"(.*)\".* ]]; then
+    link=$(echo "${BASH_REMATCH[1]}" | sed 's/" class="post-block__title__link//g')
+    echo "Link: $link"
+  fi
+done <<< "$file_content"
+
+
+rm output.txt output2.txt
